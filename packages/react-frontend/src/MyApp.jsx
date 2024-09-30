6f04a968911053745903a03a18810adef1a338ba
noledmole <noelmurti@yahoo.com>
2024-09-30 11:45:13 -0700

    Second commit of react app

--- packages/react-frontend/src/MyApp.jsx
+++ packages/react-frontend/src/MyApp.jsx
@@ -1,25 +1,13 @@
 // src/MyApp.jsx
 import React from "react";
 import Table from "./Table";
-import Form from "./Form";
 
 function MyApp() {
-  const [characters, setCharacters] = useState([]);
   return (
     <div className="container">
-      <Table 
-        characterData={characters}
-        removeCharacter={removeOneCharacter}
-      />
-      <Form />
+      <Table />
     </div>
   );
-
-  function removeOneCharacter(index) {
-    const updated = characters.filter((character, i) => {
-      return i !== index;
-    });
-    setCharacters(updated);
-  }
 }
+
 export default MyApp;
--- packages/react-frontend/src/Table.jsx
+++ packages/react-frontend/src/Table.jsx
@@ -1,46 +1,35 @@
 // src/Table.jsx
-function TableHeader() {
-  return (
-    <thead>
-      <tr>
-        <th>Name</th>
-        <th>Job</th>
-      </tr>
-    </thead>
-  );
-}
+import React from "react";
 
-function TableBody(props) {
-  const rows = props.characterData.map((row, index) => {
-    return (
-      <tr key={index}>
-        <td>{row.name}</td>
-        <td>{row.job}</td>
-        <td>
-          <button onClick={() => props.removeCharacter(index)}>
-            Delete
-          </button>
-        <td>
-      </tr>
-    );
-   }
-  );
-  return (
-      <tbody>
-        {rows}
-       </tbody>
-   );
-}
-
-function Table(props) {
+function Table() {
   return (
     <table>
-      <TableHeader />
-      <TableBody 
-        characterData={props.characterData}
-        removeCharacter={props.removeCharacter}
-      />
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Job</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>Charlie</td>
+          <td>Janitor</td>
+        </tr>
+        <tr>
+          <td>Mac</td>
+          <td>Bouncer</td>
+        </tr>
+        <tr>
+          <td>Dee</td>
+          <td>Aspiring actress</td>
+        </tr>
+        <tr>
+          <td>Dennis</td>
+          <td>Bartender</td>
+        </tr>
+      </tbody>
     </table>
   );
 }
+
 export default Table;
